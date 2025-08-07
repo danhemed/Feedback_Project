9240e9f8403c18ccd3b72ca3452588d2a8417162
Dan <danhemed123@gmail.com>
2025-08-07 15:53:24 +0300

    connect to MongoDB

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static("./public"));
 
 app.listen(port, () => {
     console.log(`server run on http://localhost:${port}`);
