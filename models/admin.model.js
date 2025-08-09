2c214365c6c9a0c60382f45e0fb89a84cd25ceb2
Dan <danhemed123@gmail.com>
2025-08-09 23:07:40 +0300

    finish to bulid the connect of admin and connect to db and get the all complaint

---
+++ models/admin.model.js
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+config();
+
+const username = process.env.USER;
+const password = process.env.PASS;
+
+export async function checkAdmin(req) {
+    const usernameReq = req.body.username;
+    const passwordReq = req.body.password;
+
+    if (username == usernameReq && password == passwordReq) {
+        return true;
+    } else {
+        return false;
+    }
+}
